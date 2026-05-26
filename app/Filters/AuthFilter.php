f3b3760d28de269dd03a3d0489f8a7ea489fc5c6
Mihaja <andriamiharisoamihaja@gmail.com>
2026-05-26 15:38:57 +0300

    authfilter creation

---
+++ app/Filters/AuthFilter.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class AuthFilter implements FilterInterface
+{
+    /**
+     * Do whatever processing this filter needs to do.
+     * By default it should not return anything during
+     * normal execution. However, when an abnormal state
+     * is found, it should return an instance of
+     * CodeIgniter\HTTP\Response. If it does, script
+     * execution will end and that Response will be
+     * sent back to the client, allowing for error pages,
+     * redirects, etc.
+     *
+     * @param RequestInterface $request
+     * @param array|null       $arguments
+     *
+     * @return RequestInterface|ResponseInterface|string|void
+     */
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        // Si pas connecté → retour login
+        if (!session()->get('user_id')) {
+            return redirect()->to('/login');
+        }
+
+        // Si la route demande 'admin' et que t'es pas admin → refusé
+        if (in_array('admin', $arguments ?? []) && session()->get('role') !== 'admin') {
+            return redirect()->to('/client/creneaux');
+        }
+    }
+
+    /**
+     * Allows After filters to inspect and modify the response
+     * object as needed. This method does not allow any way
+     * to stop execution of other after filters, short of
+     * throwing an Exception or Error.
+     *
+     * @param RequestInterface  $request
+     * @param ResponseInterface $response
+     * @param array|null        $arguments
+     *
+     * @return ResponseInterface|void
+     */
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        //
+    }
+}
