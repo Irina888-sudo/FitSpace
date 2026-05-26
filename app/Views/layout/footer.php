f65a5dcc4b6c99f825fa0e47a854b9dd7a7c12fb
SandroAndrianina <rabarijohnsandro@gmail.com>
2026-05-26 15:09:56 +0300

    Update footer.php

---
+++ app/Views/layout/footer.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="footer-public">FitSpace &copy; 2025 — Projet CodeIgniter 4 · Tous droits <span>réservés</span></div>
+</body>
+</html>
